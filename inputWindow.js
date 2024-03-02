2a870d341f093c7d321b841fc22e246415bec64f
takuma0403 <t.takuma.saga.u@gmail.com>
2024-03-02 22:37:48 +0900

    インプットウィンドウの廃止に伴うコメントアウト処理（いずれファイルごと消え失せる）

--- inputWindow.js
+++ inputWindow.js
@@ -1,86 +1,86 @@
-const { ipcRenderer } = require('electron');
+// const { ipcRenderer } = require('electron');
 
-let charactors = []
+// let charactors = []
 
-let select = document.getElementById('personSelect');
-let person = document.getElementById('person');
-let person_color = document.getElementById('person-color')
-let time = document.getElementById('time')
-let place = document.getElementById('place')
-let action = document.getElementById('action')
+// let select = document.getElementById('personSelect');
+// let person = document.getElementById('person');
+// let person_color = document.getElementById('person-color')
+// let time = document.getElementById('time')
+// let place = document.getElementById('place')
+// let action = document.getElementById('action')
 
-function recordMemo() {
-    const newCharacter = { name: person.value, color: person_color.value }
+// function recordMemo() {
+//     const newCharacter = { name: person.value, color: person_color.value }
 
-    const isNameDuplicate = charactors.some(character => character.name === newCharacter.name);
-    const isColorDuplicate = charactors.some(character => character.color === newCharacter.color);
+//     const isNameDuplicate = charactors.some(character => character.name === newCharacter.name);
+//     const isColorDuplicate = charactors.some(character => character.color === newCharacter.color);
 
-    if (!isNameDuplicate && !isColorDuplicate) {
-        charactors.push(newCharacter);
-    } else {
-        if (isNameDuplicate) {
-            alert("すでにその名前は存在しています")
-        }
-        if (isColorDuplicate) {
-            alert("すでにその色は使われています")
-        }
-    }
+//     if (!isNameDuplicate && !isColorDuplicate) {
+//         charactors.push(newCharacter);
+//     } else {
+//         if (isNameDuplicate) {
+//             alert("すでにその名前は存在しています")
+//         }
+//         if (isColorDuplicate) {
+//             alert("すでにその色は使われています")
+//         }
+//     }
 
-    // debug
-    console.log(newCharacter)
+//     // debug
+//     console.log(newCharacter)
 
-    const log = {
-        person: person.value,
-        person_color: person_color.value,
-        time: time.value,
-        place: place.value,
-        action: action.value
-    }
+//     const log = {
+//         person: person.value,
+//         person_color: person_color.value,
+//         time: time.value,
+//         place: place.value,
+//         action: action.value
+//     }
 
-    console.log(log)
-}
-
-
-let persons = ["Person1", "Person2", "Person3"];
-
-// Function to populate the select element with persons array
-function populatePersons() {
-    let select = document.getElementById("personSelect");
-    persons.forEach(function (person) {
-        var option = document.createElement("option");
-        option.value = person;
-        option.text = person;
-        select.add(option);
-    });
-}
-
-// Function to handle the selection of a person
-function selectPerson() {
-    // If a name is selected, update the input field
-    if (select.value !== "") {
-        person.value = select.value;
-    }
-}
-
-function selectPersonReset() {
-    select.value = "";
-}
-
-getMemoData()
-function getMemoData() {
-    ipcRenderer.send('request-memo-file-path');
-    ipcRenderer.on('memo-file-path-from-parent-to-mainWindow', (event, path) => {
-        if (path) {
-            const json = fs.readFileSync(path)
-            const data = JSON.parse(json)
-            console.log(data)
-        }
-        else {
-            return
-        }
-    });
-}
+//     console.log(log)
+// }
 
 
-// Call the populatePersons function when the page loads
-window.onload = populatePersons;
+// let persons = ["Person1", "Person2", "Person3"];
+
+// // Function to populate the select element with persons array
+// function populatePersons() {
+//     let select = document.getElementById("personSelect");
+//     persons.forEach(function (person) {
+//         var option = document.createElement("option");
+//         option.value = person;
+//         option.text = person;
+//         select.add(option);
+//     });
+// }
+
+// // Function to handle the selection of a person
+// function selectPerson() {
+//     // If a name is selected, update the input field
+//     if (select.value !== "") {
+//         person.value = select.value;
+//     }
+// }
+
+// function selectPersonReset() {
+//     select.value = "";
+// }
+
+// getMemoData()
+// function getMemoData() {
+//     ipcRenderer.send('request-memo-file-path');
+//     ipcRenderer.on('memo-file-path-from-parent-to-mainWindow', (event, path) => {
+//         if (path) {
+//             const json = fs.readFileSync(path)
+//             const data = JSON.parse(json)
+//             console.log(data)
+//         }
+//         else {
+//             return
+//         }
+//     });
+// }
+
+
+// // Call the populatePersons function when the page loads
+// window.onload = populatePersons;
